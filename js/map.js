510b4d901b100e2305be01365d475f5bcb3ee282
Lauren Bolton <laurenb1231@gmail.com>
2026-05-04 23:24:28 -0400

    Reupload line file to data folder; update map.js file to add all layers to map and change style

--- js/map.js
+++ js/map.js
@@ -7,7 +7,7 @@
 
 // Create a Leaflet map in the "map" div
 // setView([lat, lon], zoom)
-const map = L.map('map').setView([42.3611, --71.0571], 12);
+const map = L.map('map').setView([42.3611, -71.0571], 12);
 
 
 
@@ -19,6 +19,7 @@ const map = L.map('map').setView([42.3611, --71.0571], 12);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
 
 
 // ============================================
@@ -49,21 +50,42 @@ L.control.layers(null, {
   'Parks': polygonLayer
 }).addTo(map);
 
+// Add city boundary layer (untoggleable)
+fetch('data/city_of_boston_outline_boundary.geojson')
+  .then(res => res.json())
+  .then(data => {
 
+    L.geoJSON(data, {
+      style: {
+         fillColor: '#969896',
+          color: '#858585',
+          weight: 2,
+          fillOpacity: 0.2
+      }
+    }).addTo(map);
+  });
+
+  
 
 // ============================================
 // Step 5: Load point data (Bike Stations)
 // ============================================
+
 fetch('data/blue_bike_stations.geojson')
   .then(res => res.json())
   .then(data => {L.geoJSON(data, {
 
+      // Filter data to city of Boston  
+      filter: function(feature) {
+        return feature.properties.District === "Boston";
+    },
+
       // Style points as circle markers
       pointToLayer: function(feature, latlng) {
         return L.circleMarker(latlng, {
-          radius: 6,
-          fillColor: '#b33892',
-          color: '#ffffff',
+          radius: 4,
+          fillColor: '#f65127',
+          color: '#9c3b17',
           weight: 1,
           fillOpacity: 0.9
         });
@@ -80,6 +102,21 @@ fetch('data/blue_bike_stations.geojson')
 // Step 6: Load line data (Cycleways)
 // ============================================
 
+fetch('data/existing_bike_network_2024.geojson')
+  .then(res => res.json())
+  .then(data => {L.geoJSON(data, {
+
+      // Style lines
+      style: function(feature) {
+        return {
+          color: '#6e4415',
+          weight: 2
+        };
+      },
+    }).addTo(lineLayer);
+
+  })
+  .catch(err => console.error('Error loading cycleways:', err));
 
 
 
@@ -87,8 +124,29 @@ fetch('data/blue_bike_stations.geojson')
 // Step 7: Load polygon data (Boundary)
 // ============================================
 
-// // Add popups
-//     onEachFeature: function(feature, layer) {
-//         const name = feature.properties.name || 'Restaurant';
-//         layer.bindPopup(`<strong>${name}</strong>`);
-//     }
+fetch('data/open_space.geojson')
+  .then(res => res.json())
+  .then(data => {
+
+    // Add polygon layer
+    const geojsonLayer = L.geoJSON(data, {
+      style: function() {
+        return {
+          color: '#59765a',
+          weight: 1,
+          fill: true,
+          fillOpacity: 0.5
+        };
+      },
+
+    // Add popups
+      onEachFeature: function(feature, layer) {
+        const name = feature.properties.SITE_NAME || 'Park';
+        layer.bindPopup(`<strong>${name}</strong>`);
+      }
+    }).addTo(polygonLayer);
+
+    map.fitBounds(geojsonLayer.getBounds());
+})
+
+.catch(err => console.error('Error loading parks:', err));
